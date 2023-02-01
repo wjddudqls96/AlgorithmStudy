a5f6c9eff46f2668a46aae01fdfd9d067417baf3
jeonghui <87287223+JeongHuiKim@users.noreply.github.com>
2023-02-02 00:25:10 +0900

    [BOJ] 동물원 / 실버1

---
+++ JeongHuiKim/src/week01/동물원.java
@@ -0,0 +1,19 @@
+package week01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 동물원 {
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		// 왼쪽, 오른쪽, 없음 3개 
+		int[][] dp = new int[N][3];
+		
+		
+		
+	}
+}
