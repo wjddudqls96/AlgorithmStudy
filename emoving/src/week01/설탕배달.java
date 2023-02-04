18c5d89d5dfddbb4f071a9eb5f243c17b17642d8
dongju <leednj0113@naver.com>
2023-02-04 18:18:48 +0900

    [BOJ] 가장 긴 증가하는 부분 수열 / 실버2

--- emoving/src/week01/설탕배달.java
+++ emoving/src/week01/설탕배달.java
@@ -11,24 +11,22 @@ public class 설탕배달 {
 
 		int n = Integer.parseInt(br.readLine());
 
-		int cnt = 0;  // 봉지 개수
-		while(true) {
+		int cnt = 0; // 봉지 개수
+		while (true) {
 			if (n % 5 == 0) { // 5의 배수이면 바로 출력
 				cnt += n / 5;
 				break;
-			} 
-			else {
+			} else {
 				n -= 3; // 3씩 빼면서 5의 배수가 될때가지 봉지 하나씩 늘리면서 카운팅
 				cnt++;
 			}
-			
-			
+
 			if (n < 0) { // 정확하게 안 떨어지면 -1 출력
 				cnt = -1;
 				break;
 			}
 		}
-		
+
 		System.out.println(cnt);
 	}
 }
